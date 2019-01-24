4b1c09f3484cbc353d0a3736743ad34140a7d34d
Paul Gearo <PaulGearo@gmail.com>
2019-01-24 15:42:23 -0600

    Individual exhibits filled out

---
+++ MyOpenKouse/Exhibit.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyOpenHouse
+{
+    class Exhibit
+    {
+        public string Name { get; set; }
+        public bool FreshWater { get; set; }
+        public int NumberOfCreatures { get; set; }
+        public bool Indangered { get; set; }
+        public List<string> Descriptions { get; set; } = new List<string>();
+
+        public Exhibit(string name, bool freshwater, int numberofcreatures, bool indangered)
+        {
+            Name = name;
+            FreshWater = freshwater;
+            NumberOfCreatures = numberofcreatures;
+            Indangered = indangered;
+
+        }
+
+
+        public void ShowExhibit()
+        {
+            string answer = Program.GetUserResponse("Would you like to start the you the " + Name + "?\n");
+            if (Program.IsAffermative(answer))
+            {
+                foreach (string description in Descriptions)
+                {
+                    Console.WriteLine(description);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Okay, let's move on to the next exibit.\n");
+            }
+
+
+        }
+    }
+}
+
---
+++ MyOpenKouse/TourGuide.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyOpenHouse
+{
+    class TourGuide
+    {
+        public string Name { get; set; }
+
+        public void ShowExibit(Exhibit exibitToShow)
+        {
+            exibitToShow.ShowExhibit();
+        }
+
+        public void Speak(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
